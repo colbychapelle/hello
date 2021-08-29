e804a2ef576a24fd051cd6525e8411babd47979e
Colby Chapelle <cchapelle2@huskers.unl.edu>
2021-08-29 21:56:27 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Colby Chapelle, Michael Endacott
+ *         cchapelle2@huskers.unl.edu
+ * Date: 2020/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+
+int main(int argc, char **argv) {
+
+  printf("Electrical Engineering\n");
+
+  return 0;
+}
